7450940fed7b1ce44e4bae16f9841e0b576c462d
MasterLevi816 <oliviaransin@gmail.com>
2023-06-12 22:06:25 -0500

    bossmode

--- src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
 import org.launchcode.techjobs.persistent.models.JobData;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,12 +25,19 @@ public class ListController {
     @Autowired
     private JobRepository jobRepository;
 
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
     static HashMap<String, String> columnChoices = new HashMap<>();
 
     public ListController () {
 
         columnChoices.put("all", "All");
         columnChoices.put("employer", "Employer");
+
         columnChoices.put("skill", "Skill");
 
     }
@@ -34,20 +45,37 @@ public class ListController {
     @RequestMapping("")
     public String list(Model model) {
 
+        model.addAttribute("employers", employerRepository.findAll());
+
+        model.addAttribute("skills", skillRepository.findAll());
+        //model.addAttribute("jobs", jobRepository.findAll());
         return "list";
     }
 
     @RequestMapping(value = "jobs")
     public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam String value) {
         Iterable<Job> jobs;
+        Iterable<Employer> employers;
+        Iterable<Skill> skills;
+
         if (column.toLowerCase().equals("all")){
             jobs = jobRepository.findAll();
+            employers = employerRepository.findAll();
+
+            skills = skillRepository.findAll();
             model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+
+            model.addAttribute("employers", employers);
+            model.addAttribute("skills", skills);
+
+
         } else {
             jobs = JobData.findByColumnAndValue(column, value, jobRepository.findAll());
             model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         }
         model.addAttribute("jobs", jobs);
+
 
         return "list-jobs";
     }
